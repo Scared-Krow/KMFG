025ccd487cac1ae74bc595a1d6d80a9c5a4b14c3
Scared-Krow <calschex004@gmail.com>
2025-10-26 11:51:10 -0500

    First Build

---
+++ data/common.cmd
@@ -0,0 +1,423 @@
+[Command]
+name = "recovery"
+command = /x
+time = 1
+buffer.time = 1
+[Command]
+name = "recovery"
+command = /a
+time = 1
+buffer.time = 1
+[Command]
+name = "recovery"
+command = /y
+time = 1
+buffer.time = 1
+[Command]
+name = "recovery"
+command = /b
+time = 1
+buffer.time = 1
+
+[Command]
+name = "TagShiftBack"
+command = d
+time = 1
+buffer.time = 1
+
+[Command]
+name = "TagShiftFwd"
+command = w
+time = 1
+buffer.time = 1
+
+[Command]
+name = "x"
+command = x
+time = 1
+buffer.time = 3
+[Command]
+name = "y"
+command = y
+time = 1
+buffer.time = 3
+[Command]
+name = "z"
+command = z
+time = 1
+buffer.time = 3
+[Command]
+name = "a"
+command = a
+time = 1
+buffer.time = 3
+[Command]
+name = "b"
+command = b
+time = 1
+buffer.time = 3
+[Command]
+name = "c"
+command = c
+time = 1
+buffer.time = 3
+[Command]
+name = "start"
+command = s
+time = 1
+buffer.time = 1
+[Command]
+name = "d"
+command = d
+time = 1
+buffer.time = 3
+[Command]
+name = "w"
+command = w
+time = 1
+buffer.time = 3
+
+[Command]
+name = "menu"
+command = m
+time = 1
+buffer.time = 1
+
+[Command]
+name = "fwd"
+command = $F
+time = 1
+[Command]
+name = "back"
+command = $B
+time = 1
+[Command]
+name = "up"
+command = $U
+time = 1
+[Command]
+name = "down"
+command = $D
+time = 1
+
+
+[Command]
+name = "holdfwdB"
+command = /$F
+time = 1
+buffer.time = 3
+[Command]
+name = "holdbackB"
+command = /$B
+time = 1
+buffer.time = 3
+[Command]
+name = "holdupB"
+command = /$U
+time = 1
+buffer.time = 3
+[Command]
+name = "holddownB"
+command = /$D
+time = 1
+buffer.time = 3
+
+[Command]
+name = "holdfwd"
+command = /$F
+time = 1
+[Command]
+name = "holdback"
+command = /$B
+time = 1
+[Command]
+name = "holdup"
+command = /$U
+time = 1
+[Command]
+name = "holddown"
+command = /$D
+time = 1
+
+[Command]
+name = "holdx"
+command = /x
+time = 1
+[Command]
+name = "holda"
+command = /a
+time = 1
+[Command]
+name = "holdy"
+command = /y
+time = 1
+[Command]
+name = "holdb"
+command = /b
+time = 1
+[Command]
+name = "holdz"
+command = /z
+time = 1
+[Command]
+name = "holdc"
+command = /c
+time = 1
+[Command]
+name = "holdd"
+command = /d
+time = 1
+[Command]
+name = "holdw"
+command = /w
+time = 1
+
+;-| Double Tap |-----------------------------------------------------------
+[Command]
+name = "FF"     
+command = F, F
+time = 12
+buffer.time = 6
+
+[Command]
+name = "FADash"     
+command = ~$F, >F
+time = 8
+buffer.time = 6
+
+[Command]
+name = "BB"     
+command = B, B
+time = 12
+buffer.time = 6
+
+[Command]
+name = "BADash"     
+command = ~$B, >B
+time = 8
+buffer.time = 6
+
+[Command]
+name = "22"    
+command = ~D, D
+time = 12
+buffer.time = 6
+
+[Command]
+name = "28"    
+command = ~$D, $U
+time = 4
+buffer.time = 1
+
+
+;-SUPER INPUTS-
+
+[Command]
+name = "6246A"
+command = ~F, $D, B, $F, x
+time = 40
+buffer.time = 5
+
+[Command]
+name = "6246B"
+command = ~F, $D, B, $F, a
+time = 40
+buffer.time = 5
+
+[Command]
+name = "6246C"
+command = ~F, $D, B, $F, y
+time = 40
+buffer.time = 5
+
+[Command]
+name = "6246D"
+command = ~F, $D, B, $F, b
+time = 40
+buffer.time = 5
+
+[Command]
+name = "236236A"
+command = ~D, $F, D, $F, x
+
+[Command]
+name = "236236B"
+command = ~D, $F, D, $F, a
+
+[Command]
+name = "236236C"
+command = ~D, $F, D, $F, y
+
+[Command]
+name = "236236D"
+command = ~D, $F, D, $F, b
+
+[Command]
+name = "214214A"
+command = ~D, $B, D, $B, x
+
+[Command]
+name = "214214B"
+command = ~D, $B, D, $B, a
+
+[Command]
+name = "214214C"
+command = ~D, $B, D, $B, y
+
+[Command]
+name = "214214D"
+command = ~D, $B, D, $B, b
+
+;-NORMAL INPUTS-
+
+[Command]
+name = "214A"
+command = ~D, B,  x
+
+[Command]
+name = "214B"
+command = ~D, B,  a
+
+[Command]
+name = "214C"
+command = ~D, B,  y
+
+[Command]
+name = "214D"
+command = ~D, B,  b
+
+[Command]
+name = "236A"
+command = ~D, F,  x
+
+[Command]
+name = "236B"
+command = ~D, F, a
+
+[Command]
+name = "236C"
+command = ~D, F,  y
+
+[Command]
+name = "236D"
+command = ~D, F,  b
+
+[Command]
+name = "623A"
+command = F, D, $F,  x
+time = 20
+buffer.time = 3
+
+[Command]
+name = "623B"
+command = F, D, $F,  a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "623C"
+command = F, D, $F,  y
+time = 20
+buffer.time = 3
+
+[Command]
+name = "623D"
+command = F, D, $F,  b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "421A"
+command = B, D, $B,  x
+
+[Command]
+name = "421B"
+command = B, D, $B,  a
+
+[Command]
+name = "421C"
+command = B, D, $B,  y
+
+[Command]
+name = "421D"
+command = B, D, $B,  b
+
+[Command]
+name = "426A"
+command = ~B, D, F,  x
+
+[Command]
+name = "426B"
+command = ~B, D, F, a
+
+[Command]
+name = "426C"
+command = ~B, D, F,  y
+
+[Command]
+name = "426D"
+command = ~B, D, F,  b
+
+[Command]
+name = "624A"
+command = ~F, D, B,  x
+
+[Command]
+name = "624B"
+command = ~F, D, B, a
+
+[Command]
+name = "624C"
+command = ~F, D, B,  y
+
+[Command]
+name = "624D"
+command = ~F, D, B,  b
+
+[Command]
+name = "22A"    
+command = ~D, D, x
+
+[Command]
+name = "22B"    
+command = ~D, D, a
+
+[Command]
+name = "22C"    
+command = ~D, D, y
+
+[Command]
+name = "22D"    
+command = ~D, D, b
+
+[Command]
+name = "46A"    
+command = ~40$B, F, x
+
+[Command]
+name = "46B"    
+command = ~40$B, F, a
+
+[Command]
+name = "46C"    
+command = ~40$B, F, y
+
+[Command]
+name = "46D"    
+command = ~40$B, F, b
+
+[Command]
+name = "28A"    
+command = ~40$D, U, x
+
+[Command]
+name = "28B"    
+command = ~40$D, U, a
+
+[Command]
+name = "28C"    
+command = ~40$D, U, y
+
+[Command]
+name = "28D"    
+command = ~40$D, U, b
